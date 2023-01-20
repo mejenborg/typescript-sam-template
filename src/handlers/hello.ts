7c6891b0073031ab93aeb652fd862842913cf765
Kasper M. Sørensen <kasper.soerensen@lego.com>
2023-01-20 10:59:17 +0100

    Add prettier and eslint

--- src/handlers/hello.ts
+++ src/handlers/hello.ts
@@ -1,15 +1,8 @@
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  Context,
-} from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
-export const lambdaHandler = async (
-  event: APIGatewayProxyEvent,
-  context: Context
-): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: "Hello world",
-  };
+export const lambdaHandler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    return {
+        statusCode: 200,
+        body: 'Hello world',
+    };
 };
